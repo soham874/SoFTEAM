a255e933439fc2fad47aa7254d416bf3febd89ad
Soham Choudhury <soham874@gmail.com>
2024-01-03 20:34:32 +0530

    health endpoint

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/health')
+def health_check():
+    return 'App up and running'
+
 @app.route('/hello', methods=['GET'])
 def hello():
     return 'Hello Sohams Test app'
