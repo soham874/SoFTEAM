41fc8fd5787bd2b98ac0eb2c23b4cdac17e7c454
Soham Choudhury <soham874@gmail.com>
2024-01-05 19:21:59 +0530

    Bug fixes

--- Service/TaResultAnalyser.py
+++ Service/TaResultAnalyser.py
@@ -12,14 +12,14 @@ def generate_analysis_results(symbol):
 
     # Get today's date
     end_date = datetime.now()
-    start_date = (end_date - timedelta(days=(365 + serviceConstants.CONSIDER_DAYS_FOR_TA_REPORT))).strftime("%Y-%m-%d")
+    start_date = (end_date - timedelta(days=(365 + CommonUtils.return_config_json_value(serviceConstants.CONSIDER_DAYS_FOR_TA_REPORT)))).strftime("%Y-%m-%d")
 
     end_date = datetime.strftime(end_date, '%Y-%m-%d')
 
     start_date = pd.to_datetime(start_date)
     end_date = pd.to_datetime(end_date)
 
-    ta_data = CommonUtils.jsonify_df_object(TaService.perform_and_return_ta_data(start_date,end_date,symbol))[-serviceConstants.CONSIDER_DAYS_FOR_TA_REPORT:]
+    ta_data = CommonUtils.jsonify_df_object(TaService.perform_and_return_ta_data(start_date,end_date,symbol))[-CommonUtils.return_config_json_value(serviceConstants.CONSIDER_DAYS_FOR_TA_REPORT):]
 
     __private_add_analysis_fields(ta_data)
 
