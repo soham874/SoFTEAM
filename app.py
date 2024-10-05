57f4b6f74f9f419c5afaa4125840654b263d7c20
Soham Choudhury - s0c0kug <Soham.Choudhury@walmart.com>
2024-10-06 00:38:36 +0530

    Added live feed frontend

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 from flask_apscheduler import APScheduler
 from Service.NewsAnalysis.RssConsumer import RssConsumer
 import os, random, time, json, time
+from Common.log_config import get_logger
 
 from Controller.analysis_endp import analysis_endp
 from Controller.kite_util_endp import kite_util
@@ -9,6 +10,7 @@ from Controller.config_controller import config_handler
 import Common.constants as constants
 from Service import ConfigHandler
 
+log = get_logger(__name__)
 app = Flask(__name__)
 
 rssConsumer = RssConsumer()
@@ -20,6 +22,10 @@ app.register_blueprint(config_handler, url_prefix = '/config')
 @app.route('/health')
 def health_check():
     return "200 OK"
+
+@app.route('/live-news-feed')
+def handle_rss_field():
+    return render_template('rss_news_handler.html')  # Render your HTML file
 
 time.sleep((random.randint(1, 10) + os.getpid()) % 10)
 
@@ -36,16 +42,24 @@ if not os.path.exists('lockfile'):
                         seconds=ConfigHandler.fetch_value_from_config(constants.RSS_PARAM_FILE_PATH,"feed_check_duration_sec"))
         def refresh_feed():
             new_articles = rssConsumer.prepare_feed_data()
-            cached_articles.append(new_articles)
-            print(f"New news length -> {len(new_articles)}")
+            if new_articles:
+                cached_articles.append(new_articles)
+            log.info(f"New news length -> {len(new_articles)}")
 
         @app.route('/cached-articles')
         def get_cached_articles():
 
             def generate():
+                batch_count = 0
                 while True:
-                    if cached_articles and cached_articles[0]:
-                        yield (json.dumps(cached_articles.pop(0))+"\n").encode('utf-8')
+                    batch_identifier = f"batch_{batch_count}"
+
+                    if cached_articles:
+                        log.debug(f"publishing batch {batch_count}")
+                        yield (f"data:{json.dumps({'batch': batch_identifier, 'data': cached_articles.pop(0)})}\n\n").encode('utf-8')
+                        batch_count += 1
+                        
                     time.sleep(5)
+                    
 
             return Response(generate(), mimetype='text/event-stream')
