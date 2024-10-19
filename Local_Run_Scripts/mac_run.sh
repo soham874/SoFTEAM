cb27e015d50f69c04b13fb866026010d67cbd1a5
Soham Choudhury <73630164+soham874@users.noreply.github.com>
2024-10-19 20:41:05 +0530

    Change architecture from monolith to microservice (#7)

--- Local_Run_Scripts/mac_run.sh
+++ Local_Run_Scripts/mac_run.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 clear
 
-docker network create my-network
+DEBUG_MODE=false  # Default debug mode is false
+WORKER_COUNT=8
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --DEBUG) DEBUG_MODE="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Please install Docker and try again."
+    exit 1
+fi
+
+# Define the network name
+NETWORK_NAME="softeam-network"
+
+# Check if the network already exists
+if ! docker network ls | grep -q "$NETWORK_NAME"; then
+    echo "Creating network: $NETWORK_NAME"
+    docker network create "$NETWORK_NAME"
+else
+    echo "Network $NETWORK_NAME already exists."
+fi
 
 # Check if the redis-server container is running
 if [ "$(docker ps -q -f name=redis-server)" ]; then
@@ -29,24 +57,24 @@ else
     echo "Failed to start Redis server on port 6379."
 fi
 
-
-# Check if Docker is installed
-if ! command -v docker &> /dev/null
+if [ $DEBUG_MODE = "true" ]
 then
-    echo "Docker is not installed. Please install Docker and try again."
-    exit 1
+    echo "Starting application in debug mode with 1 worker"
+    WORKER_COUNT=1
 fi
 
 # Build the Docker image
-docker build -t nse_analyser .
+docker build --build-arg WORKER_COUNT=$WORKER_COUNT -t softeam .
 
-# Check if a container named 'nse_analyser' exists
-if [ "$(docker ps -aq -f name=nse_analyser)" ]; then
+# Check if a container named 'softeam' exists
+if [ "$(docker ps -aq -f name=softeam)" ]; then
     # If it does, stop it
-    docker stop nse_analyser
+    docker stop softeam
     # Then remove it
-    docker container rm nse_analyser
+    docker container rm softeam
 fi
 
 # Run the Docker container
-docker run --network my-network --name nse_analyser -it -p 8080:8080 nse_analyser
+docker run --network my-network --name softeam -it -p 8080:8080 -p 5678:5678 \
+    -e DEBUG_MODE=$DEBUG_MODE \
+    softeam
