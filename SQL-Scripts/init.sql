83cbcc6744cd18f0555c6b90701e641e5c154496
Soham Choudhury <soham874@gmail.com>
2024-11-22 22:33:00 +0530

    New tables

--- SQL-Scripts/init.sql
+++ SQL-Scripts/init.sql
@@ -16,3 +16,33 @@ CREATE TABLE IF NOT EXISTS softeam.trade_suggestion_analysis (
     created_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT trade_suggestion_analysis_pk PRIMARY KEY (id)
 );
+
+-- Table to store analysis results
+CREATE TABLE softeam.trade_risk_analaysis (
+	id BIGINT auto_increment NOT NULL,
+	trading_symbol varchar(100) NOT NULL,
+	buying_price FLOAT NOT NULL,
+	target_price FLOAT NOT NULL,
+	selling_price FLOAT NOT NULL,
+	trade_suggestion_analysis_id BIGINT NULL,
+	risk_parameter varchar(100) NOT NULL,
+	evaluation_result FLOAT NULL,
+	qualify_threshold BIT NOT NULL,
+	created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT trade_risk_analaysis_pk PRIMARY KEY (id)
+);
+
+-- Table to store matching logic results
+CREATE TABLE softeam.matching_logic_analysis (
+	id BIGINT auto_increment NOT NULL,
+	passed_company_name varchar(100) NOT NULL,
+	clean_company_name varchar(100) NOT NULL,
+	match_found BIT NOT NULL,
+	matched_company_name varchar(100) NOT NULL,
+	fuzzy_score INT NULL,
+	message varchar(500) NULL,
+	trading_symbol varchar(100) NULL,
+	isin varchar(100) NULL,
+	created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT matching_logic_analysis_pk PRIMARY KEY (id)
+);
